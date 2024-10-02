406e970e627214889c35fa921c882f2870890b70
somulo <mcomulosammy37@gmail.com>
2024-10-02 08:49:45 +0300

    chore: comment

--- primitives.js
+++ primitives.js
@@ -1,5 +1,5 @@
 // Creating constant variables for each of the primitive types
-
+// 
 const str = "Hello, world!"; // String primitive
 const num = 42;              // Number primitive
 const bool = true;           // Boolean primitive
