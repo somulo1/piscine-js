b6531636498a9997c47aa24ee8b20f9262de1d9a
somulo <mcomulosammy37@gmail.com>
2024-10-02 08:52:48 +0300

    chore: add runtime environment

--- primitives.js
+++ primitives.js
@@ -1,6 +1,7 @@
 // Creating constant variables for each of the primitive types
-// 
+
 const str = "Hello, world!"; // String primitive
 const num = 42;              // Number primitive
 const bool = true;           // Boolean primitive
 const undef = undefined;     // Undefined primitive
+console.log(str,num,bool,undef)
