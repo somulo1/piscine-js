f7800ebb0839d19d50fca12487ecd99f9f6c1863
somulo <mcomulosammy37@gmail.com>
2024-10-02 08:43:37 +0300

    add primitives.js

---
+++ primitives.js
@@ -0,0 +1,6 @@
+// Creating constant variables for each of the primitive types
+
+const str = "Hello, world!"; // String primitive
+const num = 42;              // Number primitive
+const bool = true;           // Boolean primitive
+const undef = undefined;     // Undefined primitive
