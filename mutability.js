69ef1dbd4aa655808527fb8300cafba111b5216f
somulo <mcomulosammy37@gmail.com>
2024-10-02 09:28:13 +0300

    feat: change variable name to person

--- mutability.js
+++ mutability.js
@@ -1,23 +1,23 @@
-// Define the original individual object (renaming from 'person' to avoid conflicts)
-const individual = {
+// Define the original person object (renaming from 'person' to avoid conflicts)
+const person = {
     name: "Samwel",
     age: 30,
     country: "US"
 };
 
-// Create clone1 and clone2 as deep copies of the individual object
-const clone1 = { ...individual };  // Spread operator to create a shallow copy
-const clone2 = Object.assign({}, individual);  // Another method to create a shallow copy
+// Create clone1 and clone2 as deep copies of the person object
+const clone1 = { ...person };  // Spread operator to create a shallow copy
+const clone2 = Object.assign({}, person);  // Another method to create a shallow copy
 
-// samePerson is a reference to the individual object (modifications to individual will affect samePerson)
-const samePerson = individual;
+// samePerson is a reference to the person object (modifications to person will affect samePerson)
+const samePerson = person;
 
-// Modify the individual object (age increased by 1, and country set to 'FR')
-individual.age += 1;
-individual.country = "FR";
+// Modify the person object (age increased by 1, and country set to 'FR')
+person.age += 1;
+person.country = "FR";
 
 // Logging the objects to verify changes
-console.log("individual:", individual);
+console.log("person:", person);
 console.log("clone1:", clone1);
 console.log("clone2:", clone2);
 console.log("samePerson:", samePerson);
