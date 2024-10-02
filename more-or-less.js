84d43213cf3948f262b992615c99ff1011e60526
somulo <mcomulosammy37@gmail.com>
2024-10-02 09:52:36 +0300

    chore: add more-or-less.js

---
+++ more-or-less.js
@@ -0,0 +1,25 @@
+// Function to add 1 to the argument
+function more(x) {
+    return x + 1;
+}
+
+// Function to subtract 1 from the argument
+function less(x) {
+    return x - 1;
+}
+
+// Function to add two arguments together
+function add(x, y) {
+    return x + y;
+}
+
+// Function to subtract the second argument from the first
+function sub(x, y) {
+    return x - y;
+}
+
+// Example usage
+console.log("more(5):", more(5));  // 6
+console.log("less(5):", less(5));  // 4
+console.log("add(5, 3):", add(5, 3));  // 8
+console.log("sub(5, 3):", sub(5, 3));  // 2
