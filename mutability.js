e41cf51c9514c787857afb5b90c0ce2deab1756c
somulo <mcomulosammy37@gmail.com>
2024-10-02 09:45:25 +0300

    chore: remove declaration

--- mutability.js
+++ mutability.js
@@ -1,24 +1,18 @@
 // Provided code (already in the environment):
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  };
-  
-  // Create clone1 and clone2 as shallow copies of the person object
-  const clone1 = { ...person };  // Using spread operator to create a shallow copy
-  const clone2 = Object.assign({}, person);  // Using Object.assign to create another shallow copy
-  
-  // samePerson is a reference to the original person object
-  const samePerson = person;
-  
-  // Modify the person object
-  person.age += 1;
-  person.country = 'FR';
-  
-  // Logging to verify the results
-  console.log("person:", person);
-  console.log("clone1:", clone1);
-  console.log("clone2:", clone2);
-  console.log("samePerson:", samePerson);
-  
+
+// Create clone1 and clone2 as shallow copies of the person object
+const clone1 = { ...person };  // Using spread operator to create a shallow copy
+const clone2 = Object.assign({}, person);  // Using Object.assign to create another shallow copy
+
+// samePerson is a reference to the original person object
+const samePerson = person;
+
+// Modify the person object
+person.age += 1;
+person.country = 'FR';
+
+// Logging to verify the results
+console.log("person:", person);
+console.log("clone1:", clone1);
+console.log("clone2:", clone2);
+console.log("samePerson:", samePerson);
