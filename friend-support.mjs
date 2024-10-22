832784242a6d7919b2b35ea343a6fe8cfa8ce2a3
somulo <mcomulosammy37@gmail.com>
2024-10-22 12:06:05 +0300

    refactor code

--- friend-support.mjs
+++ friend-support.mjs
@@ -1,6 +1,34 @@
-import http from 'node:http';
+// Import required modules
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type' : 'application/json'});
-    
+  res.setHeader('Content-Type', 'application/json');
+  const guestName = req.url.substring(1);
+  
+  if (!guestName) {
+    res.writeHead(404);
+    res.end(JSON.stringify({ error: 'guest not found' }));
+    return;
+  }
+  const filePath = path.join('guests', `${guestName}.json`);
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end(JSON.stringify({ error: 'guest not found' }));
+      } else {
+        res.writeHead(500);
+        res.end(JSON.stringify({ error: 'server failed' }));
+      }
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+});
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
