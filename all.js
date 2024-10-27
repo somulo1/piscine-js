e46a7d34472793a5caffe6b98e7760ccb3bac9f3
somulo <mcomulosammy37@gmail.com>
2024-10-27 14:47:03 +0300

    remove files

--- all.js
+++
@@ -1,8 +0,0 @@
-async function all(obj) {
-    var result = {};
-    if (Object.keys(obj).length === 0) return {};
-    for (let key in obj) {
-        result[key] = await obj[key];
-    }
-    return result;
-}
